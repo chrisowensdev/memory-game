02153fa083bf976be8e9cd4e84b836c9e885b832
Chris Owens <guitarrockstarco@gmail.com>
2020-10-07 12:10:09 -0400

    Create flip card styling and functionality using state

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import './MemoryCard.css';
 
-export class MemoryCard extends Component {
+class MemoryCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isFlipped: false };
+    }
+    clickHandler() {
+        this.setState({
+            isFlipped: !this.state.isFlipped,
+        });
+    }
+
     render() {
+        let memoryCardInnerClass = 'MemoryCardInner';
+        if (this.state.isFlipped) {
+            memoryCardInnerClass += ' flipped';
+        }
+
         return (
             <div>
-                <div className='MemoryCard'>
-                    <img
-                        src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png'
-                        alt='digital-crafts-logo'
-                    />
+                <div
+                    className='MemoryCard'
+                    onClick={this.clickHandler.bind(this)}
+                >
+                    <div className={memoryCardInnerClass}>
+                        <div className='MemoryCardBack'>
+                            <img
+                                src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png'
+                                alt='digital-crafts-logo'
+                            />
+                        </div>
+                        <div className='MemoryCardFront'>∆</div>
+                    </div>
                 </div>
             </div>
         );
